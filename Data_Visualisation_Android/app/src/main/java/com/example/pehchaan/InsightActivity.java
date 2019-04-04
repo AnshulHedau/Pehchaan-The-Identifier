de9d38a5ba486ab991dceaae3332a9f3be9cb4d1
Anshul Hedau <anshulhedau01@gmail.com>
2019-04-04 15:03:02 +0530

    Testing Pull

---
+++ Data_Visualisation_Android/app/src/main/java/com/example/pehchaan/InsightActivity.java
@@ -0,0 +1,13 @@
+package com.example.pehchaan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InsightActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insight);
+    }
+}
